5f55ef909912f587a463ef280d46148f84b75d8d
e1b20038 <e1b20038@oit.ac.jp>
2022-12-13 20:24:46 +0900

    果物データの編集の実装

--- src/main/java/oit/is/z0484/kaizi/practice05/model/FruitMapper.java
+++ src/main/java/oit/is/z0484/kaizi/practice05/model/FruitMapper.java
@@ -5,18 +5,21 @@ import java.util.ArrayList;
 import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
 
 @Mapper
 public interface FruitMapper {
 
-  @Select ("SELECT * FROM FRUIT")
+  @Select("SELECT * FROM FRUIT")
   ArrayList<Fruit> selectAllFruit();
 
-  @Select ("SELECT * FROM FRUIT WHERE ID = #{id}")
+  @Select("SELECT * FROM FRUIT WHERE ID = #{id}")
   Fruit selectById(int id);
 
-  @Delete ("DELETE FROM FRUIT WHERE ID = #{id}")
+  @Delete("DELETE FROM FRUIT WHERE ID = #{id}")
   boolean deleteById(int id);
 
+  @Update ("UPDATE FRUIT SET NAME=#{name}, PRICE=#{price} WHERE ID=#{id}")
+  void updateById(Fruit fruit);
 
 }
