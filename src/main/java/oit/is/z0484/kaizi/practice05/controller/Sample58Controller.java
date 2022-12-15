a2cb9adb5191b1bf094526832d8fb80f6540a113
e1b20038 <e1b20038@oit.ac.jp>
2022-12-16 00:10:47 +0900

    sample5-11

--- src/main/java/oit/is/z0484/kaizi/practice05/controller/Sample58Controller.java
+++ src/main/java/oit/is/z0484/kaizi/practice05/controller/Sample58Controller.java
@@ -1,7 +1,10 @@
 package oit.is.z0484.kaizi.practice05.controller;
 
+import java.io.IOException;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -10,11 +13,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
+import oit.is.z0484.kaizi.practice05.service.AsyncCount58;
+
 @RestController
 @RequestMapping("/sample58")
 public class Sample58Controller {
 
   private final Logger logger = LoggerFactory.getLogger(Sample58Controller.class);
+
+  @Autowired
+  private AsyncCount58 counter58;
 
   @GetMapping("step1")
   public SseEmitter pushCount58(@AuthenticationPrincipal UserDetails user) {
@@ -28,11 +36,20 @@ public class Sample58Controller {
     }
 
     SseEmitter emitter = new SseEmitter();
+    String role = new String();
 
     if (user.getAuthorities().toString().contains("CUSTOMER")) {
       logger.info("CUSTOMER!!");
+      role = "CUSTOMER";
     } else if (user.getAuthorities().toString().contains("SELLER")) {
       logger.info("SELLER!!");
+      role = "SELLER";
+    }
+
+    try {
+      counter58.count(emitter, role);
+    } catch (IOException e) {
+      logger.warn("Exception:" + e.getClass().getName() + ":" + e.getMessage());
     }
 
     return emitter;
